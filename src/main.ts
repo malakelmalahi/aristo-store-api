23a3edb4652e5dd838b9084d4df5c6a767ffa376
Your Name <your_email@example.com>
2026-07-20 17:14:15 +0300

    setup global swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 
 async function bootstrap() {
@@ -9,6 +10,19 @@ async function bootstrap() {
     whitelist: true,
     transform: true,
   }));
+
+  //swagger API 
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce API')
+    .setDescription('E-commerce API documentation')
+    .setVersion('1.0')
+    .addTag('e-commerce')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); //swagger path
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
